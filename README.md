4479e436fbf22f610c7c13419304f4d93f4b87db
Junbo Wang <juniwang@microsoft.com>
2017-10-25 16:57:15 +0800

    chatroom and high-frequency realtime tutorials

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SignalR-hw
+# SignalR tutorials for simple ChatRoom
+
+A very simple chat room which is built on [SignalR](http://signalr.net/)
+
+
+# More examples
+[Examples](https://github.com/SignalR/SignalR/tree/dev/samples)
---
+++ src/JSignalR.Web/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(JSignalR.Web.Startup))]
+
+namespace JSignalR.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
